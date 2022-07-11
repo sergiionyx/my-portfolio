352731d8c8c7d9ebb2072c179f2cc49296e86466
sergii-onyx <twilight.cympak@gmail.com>
2022-07-11 13:07:35 -0400

    updated resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,5 +1,5 @@
 import React from "react";
-import resume from "../assets/docs/Sergii-Onyshchenko-Resume.pdf";
+import resume from "../assets/docs/Web-Development_Full-Stack-Resume_Sergii_Onyshchenko.pdf";
 import "../styles/resume.css";
 
 function Resume() {
@@ -8,7 +8,7 @@ function Resume() {
       <h1 className="m-4">Resume</h1>
       <h3 className="resume-link">
         Download my resume(.pdf){" "}
-        <a href={resume} target="_blank" rel="noreferrer" download="Sergii-Onyshchenko-Resume">
+        <a href={resume} target="_blank" rel="noreferrer" download="Web-Development_Full-Stack-Resume_Sergii_Onyshchenko">
           here.
         </a>
       </h3>
